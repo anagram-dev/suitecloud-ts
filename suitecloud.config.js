6a5c2144734af2c1cd7b51740f7c09dab95749e4
Joaquin Gatica <me@joaquingatica.com>
2026-07-08 17:39:42 -0300

    feat: print info note when downloading or creating files

--- suitecloud.config.js
+++ suitecloud.config.js
@@ -1,6 +1,19 @@
 const { spawn } = require('node:child_process');
 const SuiteCloudJestUnitTestRunner = require('@oracle/suitecloud-unit-testing/services/SuiteCloudJestUnitTestRunner');
 
+const defaultProjectFolder = 'src';
+
+const tap = (fn) => async (args) => {
+    await fn(args);
+    return args;
+};
+
+const writeInfo = (message) => {
+    const BOLD_BLUE = '\x1b[1;34m';
+    const RESET = '\x1b[0m';
+    process.stdout.write(`${BOLD_BLUE}${message}${RESET}\n`);
+};
+
 const runNpmCommand = (command) => async () =>
     new Promise((resolve, reject) => {
         const child = spawn('npm', ['run', command], {
@@ -27,15 +40,30 @@ const test = async () => {
         // Jest configuration options.
     });
 };
-
-const tap = (fn) => async (args) => {
-    await fn(args);
-    return args;
+const logJsFolderNote = () => {
+    writeInfo(
+        `NOTE: JavaScript files are placed in the ignored "./${defaultProjectFolder}/FileCabinet" folder. If needed, move them to "./${defaultProjectFolder}/SuiteScripts".`,
+    );
 };
 
 module.exports = {
-    defaultProjectFolder: 'src',
+    defaultProjectFolder,
     commands: {
+        'file:create': {
+            beforeExecuting: tap(logJsFolderNote),
+        },
+        'file:import': {
+            beforeExecuting: tap(logJsFolderNote),
+        },
+        'file:upload': {
+            beforeExecuting: tap(build),
+        },
+        'object:import': {
+            beforeExecuting: tap(logJsFolderNote),
+        },
+        'object:update': {
+            beforeExecuting: tap(logJsFolderNote),
+        },
         'project:deploy': {
             beforeExecuting: tap(async () => {
                 await build();
